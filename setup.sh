5cce8211142aa1c23735a4b63a366dfe8e81375e
Ofentse Sibande <0512225143084@tpc.local>
2025-07-30 10:21:43 +0200

    Setting up environment

---
+++ setup.sh
@@ -0,0 +1,8 @@
+touch project_description.txt
+git init
+git add "project_description"  
+git commit -m "Setting up environment"
+git branch -M main
+git remote add origin https://github.com/ofentsesibande/Project-Navigator.git
+git push -u origin main
+echo "Environment setup"
